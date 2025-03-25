341d7c3846981ec2dc2ed556973289a31f9b19a4
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2025-03-25 13:25:38 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -47,9 +47,7 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ManufacturerListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
-        context["search_form"] = ManufacturerNameSearchForm(
-            initial={"name": name}
-        )
+        context["search_form"] = ManufacturerNameSearchForm(initial={"name": name})
         return context
 
     def get_queryset(self):
@@ -86,9 +84,7 @@ class CarListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(CarListView, self).get_context_data(**kwargs)
         model = self.request.GET.get("model")
-        context["search_form"] = CarModelSearchForm(
-            initial={"model": model}
-        )
+        context["search_form"] = CarModelSearchForm(initial={"model": model})
         return context
 
     def get_queryset(self):
@@ -126,7 +122,7 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
     queryset = Driver.objects.all()
 
-    def get_context_data(self, *,  object_list=None, **kwargs):
+    def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DriverListView, self).get_context_data(**kwargs)
         username = self.request.GET.get("username")
         context["search_form"] = DriverUsernameSearchForm(
