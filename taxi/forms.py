b59ea83b2e5f5daa1e1786bb0e6b7f873576ea2c
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2026-02-10 12:21:54 +0200

    Implemented Search forms

--- taxi/forms.py
+++ taxi/forms.py
@@ -50,3 +50,29 @@ def validate_license_number(
         raise ValidationError("Last 5 characters should be digits")
 
     return license_number
+
+class DriverSearchForm(forms.Form):
+    user_name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by User Name"}),
+    )
+
+
+class CarSearchForm(forms.Form):
+    model = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by Model"}),
+    )
+
+
+class ManufacturerSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by Name"}),
+    )
